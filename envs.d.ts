e90f10b388e1b307299c330efaf9a8d2df66a7e8
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-01-25 13:58:37 -0300

    added a new way to exports globally all envs✅

--- envs.d.ts
+++ envs.d.ts
@@ -10,5 +10,8 @@ declare namespace NodeJS {
     ENDPOINT_URL: string
     AWS_ACCESS_KEY_ID: string
     AWS_SECRET_ACCESS_KEY: string
+    MSS_NAME: string
+    S3_BUCKET_NAME: string
+    CLOUD_FRONT_DISTRIBUTION_DOMAIN: string
   }
 }
