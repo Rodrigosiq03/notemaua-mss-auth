d479b4aa9bdd937ec0fecf5fb7729479c696f3e0
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-01-27 11:28:10 -0300

    added mail sending to first access pass ✅🚀

---
+++ src/shared/services/mail_html.ts
@@ -0,0 +1,21 @@
+export const mailHtml = `<!DOCTYPE html>
+<html>
+<head>
+</head>
+<style>
+	body {
+  font-family: 'Roboto', sans-serif;
+}
+</style>
+<body>
+	<div style="background-color: #e9ecef; width: 100%; height: 640px; display: flex; flex-direction: column; align-items: center;">
+    	<img src="https://i.imgur.com/QiSEbPT.png" alt="NotemauaLogo" style="width: 200px; height: 64px; object-fit: contain; margin-top: 40px;"/>
+    	<div style="background-color: #fff; width: 88%; height: 440px; margin-top: 40px; box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.2); padding-left: 20px; padding-right: 20px;">
+        	<h1 style="font-size: 28px; margin-top: 40px;">Bem vindo ao seu primeiro acesso 💻</h1>
+            <p style="font-size: 18px; margin-top: 44px; font-weight: 300; margin-right: 20px;">Para que você continue e possa realizar a retirada dos notebooks, precisamos que você altere a sua senha! Assim conseguimos garantir os acessos corretos e evitar que algum bug ocorra.</p>
+            <p style="font-size: 18px; font-weight: 300; margin-right: 20px;">Clique no botão abaixo para trocar sua senha:</p>
+        </div>
+    </div>
+</body>
+</html>
+`
