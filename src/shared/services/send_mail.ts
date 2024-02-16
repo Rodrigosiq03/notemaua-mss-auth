63fded716bf188ae9b66ab598532a5ada41cc007
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-02-16 16:01:11 -0300

    first access email with html custom done ✅

--- src/shared/services/send_mail.ts
+++ src/shared/services/send_mail.ts
@@ -2,18 +2,22 @@ import { createTransport } from 'nodemailer'
 import envs from '../../..'
 import { mailHtml } from './mail_html'
 
-export async function sendFirstAccessMail() {
+export async function sendFirstAccessMail(ra: string, newPassword: string) {
   const transporter = createTransport({
+    host: 'smtp-mail.outlook.com',
+    port: 587,
+    secure: false,
     auth: {
       user: envs.MAIL_USER,
       pass: envs.MAIL_PASSWORD
     }
   })
+
   const mailOptions = {
-    from: envs.MAIL_USER,
-    to: 'rodrigo.dsiqueira1@gmail.com',
-    subject: 'Primeiro acesso ao Notemaua',
-    html: mailHtml
+    from: `"Contato Notemaua" <${envs.MAIL_USER}>`,
+    to: `${ra}@maua.br`,
+    subject: 'Primeiro acesso ao Notemaua!',
+    html: mailHtml(newPassword)
   }
 
   transporter.sendMail(mailOptions, (error, info) => {
