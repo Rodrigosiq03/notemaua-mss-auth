17dc6e2e203aa62b2a5d0befd4541bca785a0e73
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-02-17 00:10:17 -0300

    fixed generate password with special characters

--- src/shared/services/generate_random_password.ts
+++ src/shared/services/generate_random_password.ts
@@ -1,5 +1,5 @@
 export function generateRandomPassword(length: number) {
-  const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()-_=+[{]}\\|;:\'",<.>/?'
   let password = ''
   for (let i = 0; i < length; i++) {
     password += charset.charAt(Math.floor(Math.random() * charset.length))
