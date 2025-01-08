37957297355cf792b7a90721fa15bff5b8eae36d
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-01-07 21:56:04 -0300

    refactor: Remove unused viewmodel and usecase classes for first access, forgot password, login, and update user

--- src/modules/confirm_forgot_password/app/confirm_forgot_password_viewmodel.ts
+++
@@ -1,7 +0,0 @@
-export class ConfirmForgotPasswordViewmodel {
-  toJSON() {
-    return {
-      'message': 'Password updated successfully. You can now login.'
-    }
-  }
-}
--- src/modules/forgot_password/app/forgot_password_viewmodel.ts
+++
@@ -1,7 +0,0 @@
-export class ForgotPasswordViewmodel {
-  toJSON() {
-    return {
-      'message': 'A code has been sent to your email. Please check it.'
-    }
-  }
-}
