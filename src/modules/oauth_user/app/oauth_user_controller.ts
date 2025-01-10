b00f2e1ae715c67e848242727ce2587781393758
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2025-01-10 10:41:42 -0300

    feat: Adicionar controlador e apresentador para gerenciamento de usuários OAuth

---
+++ src/modules/oauth_user/app/oauth_user_controller.ts
@@ -0,0 +1,43 @@
+import { OAuthUserUsecase } from './ouath_user_usecase'
+
+import { IRequest } from '../../../shared/helpers/external_interfaces/external_interface'
+import { BadRequest, Created, Forbidden, InternalServerError, OK } from '../../../shared/helpers/external_interfaces/http_codes'
+import { EntityError } from '../../../shared/helpers/errors/domain_errors'
+import { MissingParameters, UserNotAllowed } from '../../../shared/helpers/errors/controller_errors'
+
+
+export class OAuthUserController {
+  usecase: OAuthUserUsecase
+
+  constructor(usecase: OAuthUserUsecase) {
+    this.usecase = usecase
+  }
+  async handle(request: IRequest)  {
+    try {
+      if (!request) {
+        throw new MissingParameters('Request')
+      }
+
+      const token: string = request.data.token as string
+
+      const response = await this.usecase.execute(token)
+
+      if (response.created_user) {
+        return new Created({token: response.token})
+      }
+      return new OK({token: response.token})     
+
+    } catch (error: any) {
+      if (error instanceof EntityError) {
+        return new BadRequest(error.message)
+      }
+      if (error instanceof UserNotAllowed) {
+        return new Forbidden(error)
+      }
+      if (error instanceof MissingParameters) {
+        return new BadRequest(error.message)
+      }
+      return new InternalServerError(error.message)
+    }
+  }
+}
