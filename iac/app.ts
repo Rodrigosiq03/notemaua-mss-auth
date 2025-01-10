20633f3e0205db2f1606e513cf12d153a5b315e8
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-01-10 00:15:59 -0300

    fix: Validate required environment variables in app configuration

--- iac/app.ts
+++ iac/app.ts
@@ -12,9 +12,14 @@ console.log('Finished adjusting the layer directory')
 
 const app = new cdk.App()
 
-const awsRegion = env.REGION
-const awsAccount = env.AWS_ACCOUNT_ID
-const stackName = env.STACK_NAME
+if (!env.REGION || !env.AWS_ACCOUNT_ID || !env.STACK_NAME) {
+  throw new Error('Missing required environment variables. AQUIIIIII AQUIIIII AQUIIIII AQUUIIIIII AQUIIIIII');
+}
+
+const awsRegion = env.REGION;
+const awsAccount = env.AWS_ACCOUNT_ID;
+const stackName = env.STACK_NAME;
+
 
 const tags = {
   'project': 'NotemauaMssAuth',
