b7d9a18bdd50d34e98bd957f30c77705eb60b813
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-01-09 22:58:10 -0300

    fix: Remove token decoding from getUserPresenter and pass request directly to controller

--- src/modules/get_user/app/get_user_presenter.ts
+++ src/modules/get_user/app/get_user_presenter.ts
@@ -2,7 +2,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Environments } from '../../../shared/environments'
 import { LambdaHttpRequest, LambdaHttpResponse } from '../../../shared/helpers/external_interfaces/http_lambda_requests'
-import { getUserFromToken } from '../../../shared/middlewares/jwt_middleware'
 import { GetUserController } from './get_user_controller'
 import { GetUserUsecase } from './get_user_usecase'
 
@@ -12,8 +11,7 @@ const controller = new GetUserController(usecase)
 
 export async function getUserPresenter(event: Record<string, any>) {
   const httpRequest = new LambdaHttpRequest(event)
-  const decoded = getUserFromToken(httpRequest.data.Authorization)
-  const response = await controller.handle(httpRequest, decoded)
+  const response = await controller.handle(httpRequest)
   const httpResponse = new LambdaHttpResponse(response?.body, response?.statusCode, response?.headers)
 
   return httpResponse.toJSON()
