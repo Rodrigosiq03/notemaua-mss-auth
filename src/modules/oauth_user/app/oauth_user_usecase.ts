7e7de5d3b866bc3a107a27cf2ce8ebed57d7fa86
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-01-13 20:39:13 -0300

    refactor: Simplificar lógica de autenticação e criação de usuários no caso de uso OAuth

--- src/modules/oauth_user/app/oauth_user_usecase.ts
+++ src/modules/oauth_user/app/oauth_user_usecase.ts
@@ -1,7 +1,11 @@
 import { IUserRepository } from '../../../shared/domain/repositories/user_repository_interface'
 import { User } from '../../../shared/domain/entities/user'
 import { TokenAuth } from '../../../shared/helpers/external_interfaces/token_auth'
-import { MissingParameters, UserNotAllowed, UserNotAuthenticated } from '../../../shared/helpers/errors/controller_errors'
+import {
+  MissingParameters,
+  UserNotAllowed,
+  UserNotAuthenticated,
+} from '../../../shared/helpers/errors/controller_errors'
 import { ROLE } from '../../../shared/domain/enums/role_enum'
 import { v4 as uuid } from 'uuid'
 
@@ -14,57 +18,45 @@ export class OAuthUserUsecase {
     this.database_repo = database_repo
   }
 
-  public async execute(token: string): Promise<{ token: string, created_user: boolean }> {
-    if (!token) {
-      throw new MissingParameters('token azure')
+  public async execute(
+    auth_code: string,
+  ): Promise<{ token: string; created_user: boolean }> {
+    if (!auth_code) {
+      throw new MissingParameters('Authorization code')
     }
 
-    const token_response = await this.token_auth.verify_azure_token(token)
-      .then(response => {
-        return response
-      }).catch(error => {
+    const access_token = await this.token_auth
+      .get_access_token(auth_code)
+      .catch((error) => {
         throw new UserNotAuthenticated(error.message)
       })
+
+    const token_response = await this.token_auth.verify_azure_token(
+      access_token,
+    )
 
     const padrao: RegExp = /@maua\.br$/
     if (!padrao.test(token_response.mail)) {
       throw new UserNotAllowed('Invalid Email, must be a maua.br domain.')
     }
 
-    let user: User
-    const get_user = await this.database_repo.getUserByEmail(token_response.mail)
-
-    if (get_user) {
-      if (!get_user.name) {
-        get_user.name = token_response.displayName.toLowerCase().split(' ').map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
-        await this.database_repo.updateUser(get_user)
-      }
-      const ra = get_user.email.split('@')[0]
-      user = new User({
-        id: get_user.id,
-        ra: ra,
-        name: get_user.name,
-        email: get_user.email,
-        role: get_user.role,
-        createdAt: get_user.createdAt,
-        updatedAt: get_user.updatedAt
-      })
-    } else {
+    let user = await this.database_repo.getUserByEmail(token_response.mail)
+    if (!user) {
       user = new User({
         id: uuid(),
         ra: token_response.mail.split('@')[0],
-        name: token_response.displayName.toLowerCase().split(' ').map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(' '), // title case
+        name: token_response.displayName,
         email: token_response.mail,
         role: ROLE.STUDENT,
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       })
       await this.database_repo.createUser(user)
     }
 
     return {
       token: await this.token_auth.generate_token(user.id),
-      created_user: !get_user
+      created_user: !user,
     }
   }
 }
