199ab6258442eec97a9a29002d97d3d9dcef0279
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2025-01-14 23:45:01 -0300

    fix: Remover variáveis de ambiente AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY_ID e CLOUD_FRONT_DISTRIBUTION_DOMAIN do arquivo de definição de ambientes

--- envs.d.ts
+++ envs.d.ts
@@ -9,13 +9,7 @@ declare namespace NodeJS {
     DYNAMO_PARTITION_KEY: string
     DYNAMO_SORT_KEY: string
     ENDPOINT_URL: string
-    AWS_ACCESS_KEY_ID: string
-    AWS_SECRET_ACCESS_KEY_ID: string
-    MSS_NAME: string
     S3_BUCKET_NAME: string
-    CLOUD_FRONT_DISTRIBUTION_DOMAIN: string
     JWT_SECRET: string
-    MAIL_USER: string
-    MAIL_PASSWORD: string
   }
 }
