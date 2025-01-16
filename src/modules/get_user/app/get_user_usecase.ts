ae408a4d4e8e86a5700ed23352534e2d40be7d0d
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2025-01-16 16:03:50 -0300

    fix: Alterar método de busca de usuário para utilizar email e lançar erro se não encontrado

--- src/modules/get_user/app/get_user_usecase.ts
+++ src/modules/get_user/app/get_user_usecase.ts
@@ -1,13 +1,18 @@
 import { User } from '../../../shared/domain/entities/user'
 import { IUserRepository } from '../../../shared/domain/repositories/user_repository_interface'
 import { EntityError } from '../../../shared/helpers/errors/domain_errors'
+import { NoItemsFound } from '../../../shared/helpers/errors/usecase_errors'
 
 export class GetUserUsecase {
   constructor(private repo: IUserRepository) {}
 
-  async execute(ra: string) {
+  async execute(email: string) {
 
-    const user = await this.repo.getUser(ra)
+    const user = await this.repo.getUserByEmail(email)
+
+    if (!user) {
+      throw new NoItemsFound('this email')
+    }
 
     return user
   }
