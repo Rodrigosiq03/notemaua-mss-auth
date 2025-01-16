4c486f5504658b9bdb28bd0f340173079a6e0d73
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2025-01-16 16:05:04 -0300

    fix: Adicionar tratamento de erro para usuário não autenticado e melhorar validação de parâmetros no OAuthUserController

--- src/modules/oauth_user/app/oauth_user_controller.ts
+++ src/modules/oauth_user/app/oauth_user_controller.ts
@@ -1,9 +1,9 @@
 import { OAuthUserUsecase } from './oauth_user_usecase'
 
 import { IRequest } from '../../../shared/helpers/external_interfaces/external_interface'
-import { BadRequest, Created, Forbidden, InternalServerError, OK } from '../../../shared/helpers/external_interfaces/http_codes'
+import { BadRequest, Created, Forbidden, InternalServerError, OK, Unauthorized } from '../../../shared/helpers/external_interfaces/http_codes'
 import { EntityError } from '../../../shared/helpers/errors/domain_errors'
-import { MissingParameters, UserNotAllowed } from '../../../shared/helpers/errors/controller_errors'
+import { MissingParameters, UserNotAllowed, UserNotAuthenticated, WrongTypeParameters } from '../../../shared/helpers/errors/controller_errors'
 
 
 export class OAuthUserController {
@@ -14,19 +14,21 @@ export class OAuthUserController {
   }
   async handle(request: IRequest)  {
     try {
-      if (!request) {
-        throw new MissingParameters('Request')
-      }
+      const authCode: string = request.data.authCode as string
+      const redirectUri: string = request.data.redirectUri as string
 
-      const token: string = request.data.token as string
+      if (!authCode) throw new MissingParameters('authCode')
+      if (!redirectUri) throw new MissingParameters('redirectUri')
 
-      const response = await this.usecase.execute(token)
+      if (typeof authCode !== 'string') throw new WrongTypeParameters('authCode', 'string', typeof authCode)
+      if (typeof redirectUri !== 'string') throw new WrongTypeParameters('redirectUri', 'string', typeof redirectUri)
+
+      const response = await this.usecase.execute(authCode, redirectUri)
 
       if (response.created_user) {
-        return new Created({token: response.token})
+        return new Created({token: response.token, created_user: response.created_user, message: 'User has been created successfully'})
       }
-      return new OK({token: response.token})     
-
+      return new OK({token: response.token})    
     } catch (error: any) {
       if (error instanceof EntityError) {
         return new BadRequest(error.message)
@@ -34,7 +36,13 @@ export class OAuthUserController {
       if (error instanceof UserNotAllowed) {
         return new Forbidden(error)
       }
+      if (error instanceof UserNotAuthenticated) {
+        return new Unauthorized(error.message)
+      }
       if (error instanceof MissingParameters) {
+        return new BadRequest(error.message)
+      }
+      if (error instanceof WrongTypeParameters) {
         return new BadRequest(error.message)
       }
       return new InternalServerError(error.message)
